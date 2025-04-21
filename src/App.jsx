fe0f38f10b94f3c33f39c1aa015ea15abd98d92e
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-04-21 18:33:20 +0530

    css loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css';
 import Home from './Home'
+import SvgLoader from './SvgLoader'
+import { useEffect, useState } from 'react';
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // simulate load delay
+    const timer = setTimeout(() => setLoading(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div>
-       <Home/>
-    </div>
+    <> 
+    {loading ? (
+      <SvgLoader />
+    ) : (<div>
+      <Home/>
+   </div>)}
+   
+   </>
+    
   
   );
 }
