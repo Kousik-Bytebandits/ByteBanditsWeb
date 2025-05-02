09f9ef54ad2f4b1c2bfeef5a2b3106f670efe3dd
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-05-02 18:49:34 +0530

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import ContactPage from './ContactPage';
 import SvgLoader from './SvgLoader';
@@ -7,36 +7,29 @@ import { useEffect, useState } from 'react';
 import Scroll from './Scroll';
 import ErrorPage from './ErrorPage';
 
-const RouteWrapper = () => {
-  const location = useLocation();
-  const [loading, setLoading] = useState(true); 
+export default function App() {
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000); 
+    // simulate load delay
+    const timer = setTimeout(() => setLoading(false), 5000);
     return () => clearTimeout(timer);
-  }, [location.pathname]); 
+  }, []);
 
-  if (loading) return <SvgLoader />;
+  if (loading) {
+    return <SvgLoader />;
+  }
 
   return (
-    <div className="fade-in">
+    <div className='fade-in'>
+    <Router>
+      <Scroll/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/error" element={<ErrorPage/>}/>
       </Routes>
-    </div>
-  );
-};
-
-export default function App() {
-  return (
-    <Router>
-      <Scroll />
-      <RouteWrapper />
     </Router>
+    </div>
   );
 }
