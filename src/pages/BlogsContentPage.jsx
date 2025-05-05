4e4adb446438021201555a90342a88c6598d36fc
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-05-05 18:41:24 +0530

    mobile

--- src/pages/BlogsContentPage.jsx
+++ src/pages/BlogsContentPage.jsx
@@ -128,24 +128,24 @@ export default function BlogContentPage() {
   return (
     <div className="min-h-screen bg-white overflow-hidden">
       <Navbar/>
-      <div className="relative h-[120vh] w-100% overflow-hidden rounded-[3rem] ml-10 mr-10 shadow-lg mt-45">
+      <div className="relative blog-header-image h-[120vh] w-100% overflow-hidden rounded-[3rem] ml-10 mr-10 shadow-lg mt-45">
         <img
           src="/images/image 65.png"
           alt="Blog Header"
           className="absolute top-0 left-0 w-full h-full object-cover scale-110 rounded-2xl"
         />
-      <div  className="absolute inset-0  rounded-2xl flex items-left justify-left flex-col font-[inter] h-[330px] bg-[#65685269]/61 font-bold border-3 rounded-4xl  border-white text-white text-[80px] mt-195 mb-8 ml-10 mr-10 px-10  py-10   backdrop-blur-lg">
+      <div  className="blog-header-text absolute inset-0  rounded-2xl flex items-left justify-left flex-col font-[inter] h-[330px] bg-[#65685269]/61 font-bold border-3 rounded-4xl  border-white text-white text-[80px] mt-195 mb-8 ml-10 mr-10 px-10  py-10   backdrop-blur-lg">
         <p >10 UI Mistakes Beginners Make </p>
         <p>And How to Fix Them </p>
       </div>
 
       </div>
 
-      <div className="max-w-8xl mx-auto grid grid-cols-1 md:grid-cols-15  px-5 py-22 mt-20" ref={contentRef}>
+      <div className="max-w-8xl main-layout mx-auto grid grid-cols-1 md:grid-cols-15  px-5 py-22 mt-20" ref={contentRef}>
 
         <aside className="hidden md:block col-span-3 left-article sticky top-[80px] ">
           <div className="relative mb-4 ">
-            <h3 className="font-bold text-[18px] uppercase">In This Article</h3>
+            <h3 className="font-bold text-[18px] font-[inter] uppercase md:text-[18px]">In This Article</h3>
            
     <div className="relative w-full h-[8px] bg-gray-300 mt-1 mb-2 rounded overflow-hidden">
       <motion.div
@@ -157,7 +157,7 @@ export default function BlogContentPage() {
           <ul className="space-y-4 text-sm mt-4">
             {sections.map((title, index) => (
               <li key={index}>
-                <button onClick={() => scrollToSection(index)} className="text-[#005FA4] text-[16px] font-bold hover:underline">
+                <button onClick={() => scrollToSection(index)} className="text-[#005FA4] font-[inter] text-[14px] md:text-[16px] font-bold hover:underline">
                   {index + 1}. {title}
                 </button>
               </li>
@@ -165,12 +165,12 @@ export default function BlogContentPage() {
           </ul>
         </aside>
 
-        <div className="col-span-1 md:col-span-9 space-y-12 text-[20px] font-semibold font-[inter]">
+        <div className="main-content col-span-1 md:col-span-9 space-y-12 text-[20px] font-semibold font-[inter]">
           <p>User Interface (UI) design is a critical part of creating digital products that feel intuitive, look polished, and keep users engaged. However, beginners often fall into some common traps that can hinder usability and overall user satisfaction. If you&apos;re new to UI design, learning to spot and fix these mistakes early can save you time and elevate your work dramatically.</p>
-          <p>Here are the top 10 UI mistakes beginners make — and how to avoid them.</p>
+          <p>Here are the top 10 UI mistakes beginners make and how to avoid them.</p>
           {sections.map((title, index) => (
             <div key={index} id={`section-${index}`} className="scroll-mt-24 space-y-10">
-              <h3 className="text-[32px] font-bold text-black">
+              <h3 className="md:text-[32px] text-[20px] font-bold text-black">
                 {index + 1}. {title}
               </h3>
               <div className="space-y-2">
@@ -181,7 +181,7 @@ export default function BlogContentPage() {
                   <span className="">Why It’s a Problem:</span> {sectionContents[index].why}
                 </p>
                 <div>
-                  <p className="text-[24px] font-bold mt-8">How to Fix It:</p>
+                  <p className="md:text-[24px] text-[18px] font-bold mt-8">How to Fix It:</p>
                   <ul className="list-disc list-inside ml-4 space-y-2 ">
                     {sectionContents[index].how.map((item, i) => (
                       <li key={i}>{item}</li>
@@ -209,101 +209,106 @@ export default function BlogContentPage() {
 
 {/* Light Green Background Section */}
       <div>
-   <div className="bg-[#C7DDDB] h-[100vh] w-full pt-12 pb-32 relative">
+   <div className="bg-[#C7DDDB] h-[50vh] md:h-[100vh] w-full pt-12 pb-32 relative">
     <div className="max-w-8xl mx-auto px-6 flex items-center justify-center mt-10">
       {/* Heading */}
-      <div className="text-xl md:text-[70px] font-black text-black font-[inter]   ">
-        <p className="ml-[350px]"><span className="text-[#168372]">Don’t stop</span> now</p>
-       <p className="mt-[-25px]"> <span className="text-[#168372]">There’s more</span> where that came from.</p>
+      <div className="text-xl md:text-[70px] cta-heading font-black text-black font-[inter]   ">
+        <p className="md:ml-[350px]"><span className="text-[#168372]">Don’t stop</span> now</p>
+       <p className="md:mt-[-25px]"> <span className="text-[#168372]">There’s more</span> where that came from.</p>
       </div>
     </div>
   </div>
 
 
-  <div className="w-full -mt-150 relative z-10 h-150">
-    <div className="max-w-8xl  px-2 flex flex-col md:flex-row justify-center gap-16">
+  <div className="w-full md:-mt-150 relative z-10 md:h-150">
+    <div className="card-section max-w-8xl  px-2 flex flex-col md:flex-row justify-center gap-16">
       {/* Card 1 */}
-      <div className="bg-white  rounded-4xl shadow-md overflow-hidden w-full md:w-[700px] h-[750px] ">
+      <div className="bg-white card  md:rounded-4xl rounded-2xl shadow-md overflow-hidden w-full md:w-[700px] h-[750px] ">
         <img
           src="/images/image 67.png"
           alt="Card 1"
-          className="w-full h-100 px-4 py-4 rounded-4xl object-cover"
+          className="w-full h-100 md:px-4 md:py-4 px-1.5 py-1.5 md:rounded-4xl rounded-2xl object-cover card-img"
         />
-        <div className="py-5 ml-10">
+        <div className="md:py-5 py-2 md:ml-10 ml-5">
           <h3 className="text-[40px] font-semibold mb-2 font-[inter] leading-snug">
             From Clunky to Clean: <br />A Real-World UI Makeover <br/>Case Study
           </h3>
 
-          <p className="text-[#009a7b] text-[24px] mt-14  font-medium">Read More &gt;</p>
+          <p className="text-[#009a7b] text-[24px] md:mt-14 mt-5 font-medium">Read More &gt;</p>
         </div>
       </div>
 
       {/* Card 2 */}
-      <div className="bg-white rounded-4xl shadow-md overflow-hidden w-full md:w-[700px] md:h-[750px]">
+      <div className="bg-white card md:rounded-4xl rounded-2xl shadow-md overflow-hidden w-full md:w-[700px] md:h-[750px]">
         <img
           src="/images/image 68.png"
           alt="Card 2"
-          className="w-full h-100 px-4 py-4 rounded-4xl object-cover"
+          className="w-full h-100 md:px-4 md:py-4 px-1.5 py-1.5 md:rounded-4xl  rounded-2xl object-cover card-img"
         />
-        <div className="py-5 ml-10">
+        <div className="md:py-5 py-2 md:ml-10 ml-5">
           <h3 className="text-[40px] font-semibold font-[inter] mb-2 leading-snug">
             UI/UX Interview Questions <br />(and How to Answer Them)
           </h3>
-          <p className="text-[#009a7b] mt-27  text-[24px] font-medium">Read More &gt;</p>
+          <p className="text-[#009a7b] md:mt-27 mt-8  text-[24px] font-medium">Read More &gt;</p>
         </div>
       </div>
     </div>
   </div>
 
  
-  <div className="bg-[#0E2B32] text-white py-12 mt-60 h-[60vh] ml-10 mr-10 mb-15 rounded-4xl">
-    <div className="max-w-8xl mx-auto px-6">
-      <div className="flex flex-col md:flex-row items-start gap-10">
+  <div className="bg-[#0E2B32] text-white md:py-12 md:mt-60 mt-25 md:h-[60vh] h-[61vh] md:ml-10 md:mr-10 ml-2 mr-2 mb-15 md:rounded-4xl rounded-2xl ">
+    <div className="max-w-8xl mx-auto md:px-6">
+      <div className="flex flex-col md:flex-row items-start gap-10 ">
         {/* Left text */}
-        <div className="w-full md:w-2/5">
-          <h3 className="text-[60px] font-bold leading-tight mt-25 ml-20 font-[inter]">
-            <span className="text-[#50BD8A]">Was this helpful?</span><br />
-            Confusing?<br />
-            <span className="text-[#50BD8A]">Brilliant?</span><br />
+        <div className="w-full md:w-2/5 feed">
+          <h3 className="md:text-[60px] text-[23px] font-bold leading-wide  md:mt-25 md:ml-20 ml-7 mt-8 font-[inter]">
+            <span className="text-[#50BD8A]">Was this helpful?</span>
+            Confusing?
+            <span className="text-[#50BD8A]">Brilliant?</span>
             Let me know!
           </h3>
         </div>
 
         {/* Right form */}
-        <div className="w-900px md:w-2/5 font-[inter] font-bold text-[25px] ">
-          <form className="space-y-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-15 ">
-              <label >Nick Name
-              <input
-                type="text"
-               
-                className="p-2 rounded-lg mt-3 mb-5  bg-[#E5FFF3] text-black w-full "
-              />
-              </label>
-             <label>Email
-              <input
-                type="email"
-               
-                className="p-2 rounded-lg mt-3 mb-5  bg-[#E5FFF3] text-black  w-[400px]"
-              />
-              </label>
-            </div>
-            <label>Suggestions
-            <textarea
-              rows="4"
-             
-              className="w-[760px] h-[200px] p-2 rounded-lg mt-3 mb-5  bg-[#E5FFF3] text-black"
-            ></textarea>
-            </label>
-            <button
-              type="submit"
-              className="bg-[#E5FFF3] text-[#0E2B32] px-20  py-2 rounded-4xl hover:bg-[#a6d1c2] transition  font-bold ml-60"
-            >
-              SEND
-            </button>
-          </form>
-        </div>
+        <div className="w-[380px] md:w-[900px] md:w-2/5 font-[inter] font-bold md:text-[25px] text-[12px]">
+  <form className="space-y-4">
+    {/* Name and Email - Row on small screens */}
+    <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4 px-8 md:px-0">
+      <div className="flex flex-col ">
+        <label className="mb-1">Nick Name</label>
+        <input
+          type="text"
+          className="p-2 rounded-lg bg-[#E5FFF3] text-black md:w-full w-[320px]"  />
       </div>
+      <div className="flex flex-col">
+        <label className="mb-1">Email</label>
+        <input
+          type="email"
+          className="p-2 rounded-lg bg-[#E5FFF3] text-black md:w-full w-[320px]"
+        />
+      </div>
+    </div>
+
+    {/* Suggestions */}
+    <div className="flex flex-col px-8 md:px-0">
+      <label className="mb-1">Suggestions</label>
+      <textarea
+        rows="4"
+        className=" md:w-full w-[320px] md:h-[200px] h-[150px] p-2 rounded-lg bg-[#E5FFF3] text-black"
+      ></textarea>
+    </div>
+
+    {/* Submit Button */}
+    <button
+      type="submit"
+      className="bg-[#E5FFF3] text-[#0E2B32] md:px-20 px-12 py-2 rounded-4xl hover:bg-[#a6d1c2] transition font-bold mx-auto block md:ml-70"
+    >
+      SEND
+    </button>
+  </form>
+</div>
+
+        </div>
     </div>
   </div>
 </div>
