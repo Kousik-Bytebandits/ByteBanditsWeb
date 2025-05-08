d8bc5f248d912cdbf96d28d5fc80e7ef58f695b6
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-05-08 18:12:09 +0530

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import '../src/styles/index.css'
 import App from './App'
 import ScrollButton from '../src/components/ScrollButton'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
     <ScrollButton/>
   </StrictMode>,
 )
